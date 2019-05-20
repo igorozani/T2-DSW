a9b94f9341222d4a51505f2372d6e57cf792988d
igorozani <33789352+igorozani@users.noreply.github.com>
2019-05-20 11:46:18 -0300

    Add files via upload

--- T2/src/java/br/ufscar/dc/dsw/SitesDeVendas/pojo/Cadastro.java
+++ T2/src/java/br/ufscar/dc/dsw/SitesDeVendas/pojo/Cadastro.java
@@ -1,14 +1,83 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufscar.dc.dsw.SitesDeVendas.pojo;
 
-/**
- *
- * @author 725815
- */
-public class Cadastro {
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Cadastro implements Serializable{
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String email;
+    private String senha;
+    private String cnpj;
+    private String nome;
+    private String cidade;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="cadastrado", nullable=false)
+    private Usuario cadastrado;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public String getCNPJ() {
+        return cnpj;
+    }
+
+    public void setCNPJ(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    
+    public Usuario getCadastrado() {
+        return cadastrado;
+    }
+
+    public void setCadastrado(Usuario cadastrado) {
+        this.cadastrado = cadastrado;
+    }
 }
--- T2/src/java/br/ufscar/dc/dsw/SitesDeVendas/pojo/Usuario.java
+++ T2/src/java/br/ufscar/dc/dsw/SitesDeVendas/pojo/Usuario.java
@@ -1,14 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufscar.dc.dsw.SitesDeVendas.pojo;
 
-/**
- *
- * @author 725815
- */
-public class Usuario {
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Usuario implements Serializable {
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String url;
+    private String senha;
+    @Temporal(value = TemporalType.DATE)
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getURL() {
+        return url;
+    }
+
+    public void setURL(String url) {
+        this.url = url;
+    }
+    
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
